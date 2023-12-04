dddc477a665af9c3145aefd7ccdb1bbf923b9fe1
Adam <martia123456789@gmail.com>
2023-12-03 23:26:53 -0600

    Fix Directory

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/championhub/',
+  base: '/ChampionHub/',
   plugins: [vue()],
   resolve: {
     alias: {
