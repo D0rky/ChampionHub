9f232e0d22e349b726a2a665c5d7d9bbc6ada960
Adam <martia123456789@gmail.com>
2023-12-13 20:11:28 -0600

    Detail page edited

--- src/views/ChampionDetails.vue
+++ src/views/ChampionDetails.vue
@@ -17,28 +17,31 @@ onUnmounted(() => {
 </script>
 
 <template>
-    <main class="min-h-screen bg-gradient-to-r from-fuchsia-900 to-red-700 py-8 text-white">
-        <div v-if="currentChampion" class="flex flex-col items-center gap-6">
-            <!-- Character's Image -->
-            <img :src="currentChampion.icon" :alt="currentChampion.name" class="h-40 w-40 rounded-full shadow-lg" />
+    <main class="min-h-screen bg-gray-500 py-8">
+        <div v-if="currentChampion" class="container mx-auto max-w-3xl">
+            <div class="flex flex-col md:flex-row items-center gap-6 bg-white rounded-lg p-8 shadow-md">
+                <!-- Character's Image -->
+                <img :src="currentChampion.icon" :alt="currentChampion.name" class="h-40 w-40 rounded-full shadow-lg" />
 
-            <div class="text-center">
-                <!-- Character's Title, ID, and Tags -->
-                <h1 class="text-4xl font-bold">{{ currentChampion.value.title }}</h1>
-                <p class="text-white-600 text-xl">ID: {{ currentChampion.id }}</p>
-                <p class="text-white-600 text-xl">Tag: {{ currentChampion.tags }}</p>
+                <div class="text-center md:text-left">
+                    <!-- Character's Title, ID, and Tags -->
+                    <h1 class="text-4xl font-bold">{{ currentChampion.name }}</h1>
+                    <p class="text-gray-600 text-lg">ID: {{ currentChampion.title }}</p>
+                    <p class="text-gray-600 text-lg">Tag: {{ currentChampion.tags }}</p>
+                </div>
             </div>
 
             <!-- Character's Stats and Description -->
-            <div class="grid grid-cols-2 gap-4 rounded-lg bg-white p-4 shadow-lg">
-                <div>
-                    <p class="text-lg text-gray-600">Stats: {{ currentChampion.stats }}</p>
+            <div class="bg-white mt-8 p-6 rounded-lg shadow-md">
+                <div class="mb-6">
+                    <h2 class="text-2xl font-bold mb-2">Stats</h2>
+                    <p class="text-gray-700">{{ currentChampion.stats }}</p>
                 </div>
                 <div>
-                    <p class="text-lg text-gray-600">Description: {{ currentChampion.description }}</p>
+                    <h2 class="text-2xl font-bold mb-2">Description</h2>
+                    <p class="text-gray-700">{{ currentChampion.description }}</p>
                 </div>
             </div>
-
         </div>
     </main>
 </template>
